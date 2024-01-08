23971ac2ff81f5a5abf3099b7c87233fd68c7bc1
Jawid Fakhari <jawid_fakhari@live.it>
2024-01-08 16:03:54 +0100

    check Answer, disable button click, score counter Added

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 "use strict";
-//selectors
+//selectors ///////////////
 const selectors = {
   question: document.querySelector("#question"),
   answerBtns: document.querySelector("#answer-buttons"),
   btns: document.querySelectorAll(".btn"),
   nextBtn: document.querySelector("#next-btn"),
 };
-//quiz questions and answers
+//quiz data ////////////////
 const quizData = [
   {
     question: "What is the role of CSS in web development?",
     options: [
-      "Styling and formatting HTML content",
       "Defining database structures",
+      "Styling and formatting HTML content",
       "Creating interactive functions using JavaScript",
       "Optimizing server queries",
     ],
@@ -20,15 +20,15 @@ const quizData = [
   },
   {
     question: "Which selector targets all elements of a specific type in CSS?",
-    options: ["*", ".class", "#id", "!important"],
+    options: [".class", "#id", "!important", "*"],
     answer: "*",
   },
   {
     question: 'What does the acronym "CSS" stand for?',
     options: [
-      "Cascading Style Sheets",
       "Computer Style Sheets",
       "Creative Style System",
+      "Cascading Style Sheets",
       "Colorful Style Structure",
     ],
     answer: "Cascading Style Sheets",
@@ -44,10 +44,41 @@ const quizData = [
     answer: "color: red;",
   },
 ];
+//functions//////////////
+let arrIndex = 0;
+let score = 0;
+const showData = function (dataArr) {
+  if (arrIndex < dataArr.length) {
+    selectors.question.innerHTML = `${dataArr[arrIndex].question}`;
+    for (let i = 0; i < dataArr[arrIndex].options.length; i++) {
+      for (let j = 0; j < selectors.btns.length; j++) {
+        selectors.btns[j].innerHTML = `${dataArr[arrIndex].options[i++]}`;
+      }
+    }
+  } else {
+    selectors.nextBtn.style.display = "none";
+    selectors.btns.forEach((btn) => {
+      btn.disabled = true;
+    });
+  }
+};
+showData(quizData);
 
-//functions
-
-//handlers
+const controlNext = function (input) {
+  const calssLisstArr = Array.from(selectors.btns).map((btn) =>
+    Array.from(btn.classList)
+  );
+  const activeBtnIndx = calssLisstArr.findIndex((classes) =>
+    classes.includes("active")
+  );
+  const checkAnswer = selectors.btns[activeBtnIndx].innerHTML;
+  if (checkAnswer === quizData[arrIndex].answer) {
+    arrIndex++;
+    score++;
+  }
+  console.log(score);
+};
+//handlers ///////////////////
 selectors.btns.forEach((btn) => {
   btn.addEventListener("click", function () {
     selectors.btns.forEach((otherBtn) => {
@@ -60,4 +91,9 @@ selectors.btns.forEach((btn) => {
     }
     selectors.nextBtn.style.display = "block";
   });
+});
+
+selectors.nextBtn.addEventListener("click", () => {
+  controlNext();
+  showData(quizData);
 });
